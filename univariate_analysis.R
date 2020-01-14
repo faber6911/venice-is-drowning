9262687d5e0aec06c5e280a6bf549b8e3ca4f03c
Fabrizio D'Intinosante <fabrizio_dintinosante@yahoo.it>
2020-01-15 00:40:36 +0100

    changes

--- univariate_analysis.R
+++ univariate_analysis.R
@@ -11,7 +11,8 @@ library(forecast)
 library(tseries)
 library(MLmetrics)
 library(urca)
-
+library(Hmisc)
+library(plotly)
 
 
 acfpacf <- function(x, max.lag=36){
@@ -24,46 +25,25 @@ acfpacf <- function(x, max.lag=36){
 
 # import data --------------------------------------------------------------
 
-data <- read.csv("data/output/venezia.csv", header = T)
+data <- read.csv("data/output/df_final2010-2018.csv", header = T)
+lunar_motion <- read.csv("moon_distance/moon_distances.csv", header = T)
+
 head(data)
+tail(data)
 str(data)
+describe(data)
 
-
-#data <- as.data.frame(data)
-#data$datetime <- as.character(data$datetime)
-#head(data)
-#anyNA(data$datetime)
-
-data$datetime <- as.POSIXct(data$datetime,
-                            format = "%Y-%m-%d %H:%M:%OS",
-                            tz =  "Europe/Berlin")
-head(data)
-anyNA(data)
-# mancano una serie di datetime, tutti alle 2.00 del mattino, deve esserci stato un qualche errore, sistemiamo
-imp <- paste0(date(data$datetime[as.numeric(rownames(data[is.na(data$datetime),]))-1]), " 02:00:00")
-data$datetime[as.numeric(rownames(data[is.na(data$datetime),]))] <- imp
-anyNA(data)
-
-
-data$datetime <- as.POSIXct(data$datetime,
-                            format = "%Y-%m-%d %H:%M:%OS",
-                            tz =  "Europe/Berlin")
+head(lunar_motion)
+tail(lunar_motion)
+str(lunar_motion)
+describe(lunar_motion)
 
 anyNA(data)
-
-# risolto
+anyNA(lunar_motion)
 
 # inspections -------------------------------------------------------------
 
-
-# date <- seq(from=as.POSIXct(data$datetime[1],
-#                             format = "%Y-%m-%d %H:%M:%OS",
-#                             tz = "Europe/Berlin"),
-#             length.out = nrow(data),
-#             by = "1 hour")
-# tail(date)
-
-data_ts <- xts(order.by = data$datetime,
+data_ts <- xts(order.by = as.POSIXct(data$datetime),
                x = data$level,
                frequency = 24,
                tzone = "Europe/Berlin")
@@ -76,26 +56,14 @@ ggtsdisplay(data_ts["2015-12-31 23:00:00/2018-12-31 23:00:00"],
             lag.max = 72,
             theme = theme_bw()) # 2016-2018
 
-
-df <- data_ts["2015-12-31 23:00:00/2018-12-31 23:00:00"]
-length(index(df)) == length(seq(from = start(df), to = end(df), by = "hour"))
-
-length(index(df))
-
-date <- seq(from = start(df),
-            length.out = length(index(df)),
-            by = "1 hour")
-
+rm(data_ts)
 
 # check for stationarity in variance ----------------------------------------
 
 # convertiamo in df solo per verificare stazionarietà in varianza
 
-ddf <- data.frame(level = coredata(df), datetime = index(df))
-head(ddf)
-
-ddf %>%
-  add_column(date = date(ddf$datetime)) %>%
+data %>%
+  add_column(date = date(data$datetime)) %>%
   group_by(date) %>% 
   mutate(mean_day = mean(level), sd_day = sd(level)) %>%
   select(date, mean_day, sd_day) %>% 
@@ -115,48 +83,45 @@ ddf %>%
 
 # verifichiamo la stazionarietà in media
 
-ddf %>% 
-  ggplot(aes(y = level, x = datetime)) +
+data %>% 
+  ggplot(aes(y = level, x = seq(1:nrow(data)))) +
   geom_line() +
-  geom_hline(yintercept = mean(ddf$level),
+  geom_hline(yintercept = mean(data$level),
              linetype = "dashed",
              color = "darkred",
              size = 1)  +
   annotate("label",
            label = paste0("Mean = ",
-                          round(mean(ddf$level), 2)),
+                          round(mean(data$level), 2)),
            y = 150,
-           x = as.POSIXct("2016-03-01 23:00:00"),
+           x = 3500,#as.POSIXct("2010-01-01 00:00:00"),
            size = 5,
            colour = "black") +
-  labs(title = "Tides data for 2016-2018 years with mean (in red)",
+  labs(title = "Tides data for 2010-2018 years with mean (in red)",
        x = "Datetime",
        y = "Level") +
   theme_bw()
 
-# plot(ddf$level, type = "l")
-# abline(h = mean(coredata(df)), col = "red", lty = 2, lwd = 0.8)
-
 # a prima vista sembrerebbe essere stazionaria ma possiamo testare questa impressione
 
 
-test_stat <- ur.df(ddf$level,
+test_stat <- ur.df(data$level,
                    type = "drift",
                    lags = 24,
                    selectlags = "AIC")
 summary(test_stat)
 
-
+rm(test_stat)
 # anche il test conferma essere stazionaria
 
-ddf %>%
+data %>%
   ggplot(aes(x = level, y = ..density..)) +
   geom_histogram(colour = "black",
                  fill = "lightblue",
                  bins = 50) + 
   geom_density(fill = "red",
                alpha = .2) +
-  geom_vline(xintercept = mean(ddf$level),
+  geom_vline(xintercept = mean(data$level),
              linetype = "dashed",
              color = "darkred",
              size = 1) +
@@ -170,24 +135,21 @@ ddf %>%
            size = 5) +
   theme_bw()
 
-
-# hist(ddf$level, prob = TRUE, col = "lightblue", main = "Tides density plot", xlab = "Level")
-# lines(density(ddf$level), col = "red", lwd = 2, lty = 2)
-# abline(v = mean(ddf$level), col = "blue", lwd = 2, lty = 2)
-
-
 # il fenomeno sembrerebbe distribuirsi normalmente
 
-# define train and test set -----------------------------------------------
-trn <- ddf[1:(nrow(ddf)-168),] # train sono 3 anni meno 1 settimana
-head(trn)
-tst <- ddf[(nrow(ddf)-168):nrow(ddf),] # test 1 settimana
+# define train and test set ed integrare ciclo lunare -----------------------------------------------
+data$l_motion <- lunar_motion$dists.Km.
+rm(lunar_motion)
 
+trn <- data[1:(nrow(data)-168),] # train sono 3 anni meno 1 settimana
+head(trn)
+tail(trn)
+tst <- data[(nrow(data)-168):nrow(data),] # test 1 settimana
 head(tst, 1)
 tail(tst, 1)
 
 ggtsdisplay(trn$level,
-            main = "Venice tides 01/2016 12/2018",
+            main = "Venice tides 01/2010 12/2018",
             lag.max = 72,
             theme = theme_bw())
 
@@ -199,77 +161,117 @@ ggtsdisplay(tst$level,
 # a giudicare dai grafici ACF e PACF sembrerebbe trattarsi di un modello MA stagionale
 # poichè PACF decresce geometricamente mentre ACF presenta dei picchi stagionali
 
-#diff_level <- diff(ddf$level, 672)
+library(TSA)
+p <- periodogram(data$level)
+dd <- data.frame(freq=p$freq, spec=p$spec, time=1/p$freq)
+order <- dd[order(-dd$spec),]
+top10 <- head(order, 10)
+top10
 
-# plot(tst$level, type = "l")
-# freq <- outer(1:169, 1:2)*2*pi/24
-# freq2 <- outer(1:169, 1:2)*2*pi/12
-# freq3 <- outer(1:26137, 1:2)*2*pi/400
-# 
-# plot(rowSums(cbind(sin(freq), cos(freq))), type = "l")
-# lines(rowSums(sin(freq2)), col = "red")
-# length(trn$level)
-# length(rowSums(sin(freq)))
-# length(tst$level)
-# 
-# max(trn$level)
-# min(trn$level)
-# 
-# plot(trn$level[1:(168*16)], type = "l")
-# mod1 <- lm(trn$level ~ rowSums(cbind(sin(freq), cos(freq), sin(freq2), cos(freq2))))
-# summary(mod1)
-# plot(mod1$residuals, type = "l")
-# ggtsdisplay(mod1$residuals)
+rm(list = c("p", "dd", "order", "top10"))
 
-# plot(trn$level, type = "l")
-# plot(trn_moon$distance, type = "l")
-# 
-# moon <- read.table("D:/GIT/venice-is-drowning/moon_distance/moon_distances.csv", quote="\"", comment.char="")
-# plot(moon$V1,  type = "l")
-# 
-# trn_moon <- moon[1:(nrow(moon)-168),]
-# tst_moon <- moon[(nrow(moon)-168):nrow(moon),]
-# #trn_moon$datetime <- trn$datetime
-# trn_moon <- data.frame("distance" = trn_moon)
-# tst_moon <- data.frame("distance" = tst_moon)
-# 
-# trn_moon$datetime <- trn$datetime
-# tst_moon$datetime <- tst$datetime
-# 
-# ggtsdisplay(trn_moon$distance)
-# 
-# reg <- 1/(trn_moon$distance)
-# length(trn$level)
-# 
-# mod_lin <- lm(diff(trn$level, 12) ~ diff(trn_moon$distance, 12))
-# 
-# summary(mod_lin)
-# 
-# plot(mod_lin$residuals, type = "l")
-# 
-# ggtsdisplay(trn$level)
-# ggtsdisplay(mod_lin$residuals)
-# 
-# 
-# plot(1/trn_moon$distance^2, type = "l")
-# plot(tst$level, type = "l")
+# è palese da ciò come siano presenti due stagionalità, una su 12 lag e una 24
+
+# linear models -----------------------------------------------------------
+# è necessario verificare l'impatto delle variabili esterne sulla predizione del livello delle maree
+# prima di procedere a realizzare un modello ARMA o ARIMA
+
+# solo variabili meteo additive con ciclo lunare normalizzato
+
+rain_norm <- as.vector(scale(trn$rain))
+vel_wind_norm <- as.vector(scale(trn$vel_wind))
+dir_wind_norm <- as.vector(scale(trn$dir_wind))
+l_motion_norm <- as.vector(scale(trn$l_motion))
+trn_norm <- data.frame(level = trn$level, rain = rain_norm, vel_wind = vel_wind_norm, dir_wind = dir_wind_norm,
+                       l_motion = l_motion_norm)
+
+rm(list = c("trn", "rain_norm", "vel_wind_norm", "dir_wind_norm", "l_motion_norm"))
+
+mod1 <- lm(formula = level ~ rain + 
+             vel_wind +
+             dir_wind + 
+             l_motion,
+           data = trn_norm)
+summary(mod1) # R2 3.8%
+
+plot(trn$level, type = "l")
+lines(mod1$fitted.values, col = "red")
+abline(h = mean(trn$level), col = "blue", lty = 2, lwd = 2)
+
+acfpacf(mod1$residuals)
+
+rain_norm_test <- as.vector(scale(tst$rain))
+vel_wind_norm_test <- as.vector(scale(tst$vel_wind))
+dir_wind_norm_test <- as.vector(scale(tst$dir_wind))
+l_motion_norm_test <- as.vector(scale(tst$l_motion))
+tst_norm <- data.frame(level = tst$level, rain = rain_norm_test, vel_wind = vel_wind_norm_test,
+                       dir_wind = dir_wind_norm_test,
+                   l_motion = l_motion_norm_test)
+
+rm(list = c("tst", "rain_norm_test", "vel_wind_norm_test", "dir_wind_norm_test", "l_motion_norm_test"))
+head(tst_norm)
+nrow(tst_norm)
+
+pred <- predict.lm(mod1, newdata = tst_norm, interval = 'prediction')
+nrow(pred)
+head(pred)
+
+plot(tst_norm$level, type = "l")
+lines(pred[,1], col = "red")
+abline(h = mean(tst_norm$level), col = "blue", lty = 2, lwd = 2)
+
+# variabili meteo e ciclo lunare con interazioni fittato con metodo stepwise basato su AIC
+
+mod2 <- lm(level ~ ., data = trn_norm)
+final_model <- step(mod2, scope = . ~ .^2, direction = 'both')
+summary(final_model) #R2 4.0%
+
+pred <- predict.lm(final_model, newdata = tst_norm, interval = 'prediction')
+nrow(pred)
+head(pred)
+
+
+plot(tst_norm$level, type = "l")
+lines(pred[,1], col = "red")
+abline(h = mean(tst_norm$level), col = "blue", lty = 2, lwd = 2)
+
+rm(list = c("data", "final_model", "mod1", "mod2", "pred"))
+## al netto di queste prove possiamo vedere come le covariate non sembrino spiegare molto.. proviamo con modelli ARIMA
 
 # model 1 -----------------------------------------------------------------
-acfpacf(trn$level)
 
-mod1 <- Arima(trn$level, c(0,1,0), list(order = c(2,1,2), period = 24))
-summary(mod1)
-# mod1$coef
-# sqrt(mod1$var.coef)
-# names(mod1)
-acfpacf(mod1$residuals, max.lag = 72)
+# i regressori se inseriti all'interno del modello ARIMA sembrerebbero non essere proprio
+# significativi
 
+acfpacf(trn_norm$level, max.lag = 800)
+
+lines(as.numeric(trn_norm$l_motion+trend$fitted.values), col = "red")
+##TODO applicare lm ai residui di arima
+xreg <- matrix(c(trn_norm$rain, trn_norm$vel_wind,
+         trn_norm$dir_wind, trn_norm$l_motion), ncol = 4)
+col_names <- c("rain", "vel_wind", "dir_wind", "l_motion")
+colnames(xreg) <- col_names
+
+mod1_ar <- Arima(trn_norm$level, #xreg = xreg,
+                 include.drift = T,
+                 c(2,1,1), list(order = c(3,1,3), period = 6))
+summary(mod1_ar)
+acfpacf(mod1_ar$residuals, max.lag = 72)
+Box.test(mod1_ar$residuals, type="Ljung-Box")
+pred <- forecast(mod1_ar, h = 168)
+ggplotly(autoplot(pred))
+
+plot(trn_norm$level[1:168], type = "l")
+lines(mod1_ar$fitted[1:168], col = "red")
 
 # model auto --------------------------------------------------------------
-mod_auto <- auto.arima(ts(ddf$level, frequency = 24), stepwise = F)
+mod_auto <- auto.arima(ts(trn_norm$level, frequency = 6), stepwise = T)
 summary(mod_auto)
 # auto arima trova modello (3,0,2)(0,1,0)[24]
 acfpacf(mod_auto$residuals, max.lag = 72)
+Box.test(mod_auto$residuals, type = "Ljung-Box")
+pred <- forecast(mod_auto, h = 168)
+ggplotly(autoplot(pred))
 
 mod2 <- auto.arima(ddf$level)
 summary(mod2)
@@ -283,7 +285,6 @@ acfpacf(mod2$residuals)
                         #########################################################
                         #  ############# DA QUI IN POI DEPRECATO ############## #
                         #########################################################
-
 
 plot(forecast(mod1, h = 168))
 pred <- forecast(mod1, h = 24)
@@ -344,7 +345,6 @@ test
 plot(data_ts["2018-12-01/"])
 
 
-# SARIMA ------------------------------------------------------------------
 
 # test for stationarity in mean
 adf.test(train)
@@ -383,7 +383,6 @@ mse_mod/mse_zero
 
 
 
-# regression --------------------------------------------------------------
 # regressione con variabili meteo e sinusoidi deterministiche
 
 freq  <- outer(1:nrow(data), 1:16) * 2 * pi / 365.25*12
@@ -401,7 +400,6 @@ print(which(data$datetime == "2002-12-01 00:00:00"))
 print(which(data$datetime == "2002-12-02 00:00:00"))
 
 
-# linear model ------------------------------------------------------------
 
 mod1 <- lm(level~
              dir_wind*vel_wind + rain + rain:dir_wind:vel_wind 
@@ -431,7 +429,6 @@ acfpacf(mod1$residuals)
 
 
 
-# ARIMAX ------------------------------------------------------------------
 
 reg <- cbind(data$dir_wind, data$vel_wind, data$rain)
 mod_ar <- arima(data$level, order = c(1,0,0), xreg = reg)
@@ -448,7 +445,6 @@ lines(arimax_fitted[174556:174629], col = 'green')
 legend(1, 130, legend=c("Ground Truth", "Linear", "Arimax"),
        col=c("black", "blue", 'green'), lty=1, cex=0.8)
 
-# Auto ARIMAX -------------------------------------------------------------
 
 mod_auto <- auto.arima(data$level, xreg = reg)
 auto_arimax_fitted <- mod_auto$residuals+data$level
